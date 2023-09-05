60a5682d6c56b4b2f686e38c37c90140f6df829d
Xacarami <gabriel.plautz2018@gmail.com>
2023-09-05 16:29:07 -0300

    simplificando a estilização

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { createContext, useState } from "react";
+import "./App.css";
+import Pesquisa from "./components/Pesquisa";
+import ReactSwitch from "react-switch";
+
+export const ThemeContext = createContext(null);
+
+function App() {
+    const [tema, setTema] = useState("dark");
+
+    const trocarTema = () => {
+        setTema((temaAtual) => (temaAtual === "light" ? "dark" : "light"));
+    };
+
+    return (
+        <>
+            <ThemeContext.Provider value={{ tema, trocarTema }}>
+                <div id={tema}>
+                    <div className="switch">
+                        <label>
+                            {tema === "light" ? "Modo Claro" : "Modo Escuro"}
+                        </label>
+                        <ReactSwitch
+                            onChange={trocarTema}
+                            checked={tema === "dark"}
+                        />
+                    </div>
+                    <div className="pesquisaComponent">
+                        <Pesquisa tema={tema} />
+                    </div>
+                </div>
+            </ThemeContext.Provider>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Pesquisa.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import imagem from "../assets/imagem capa.png";
+import "./Pesquisa.css";
+import DadosEncontrados from "./DadosEncontrados";
+
+function Pesquisa(tema) {
+    const [nickname, setNickname] = useState("");
+    const [pesquisa, setPesquisa] = useState([]);
+
+    function pegarNick() {
+        const nick = pesquisa.target.value;
+        setNickname(nick);
+    }
+
+    document.addEventListener(
+        "keypress",
+        function (e) {
+            //ao apertar enter, é enviado o texto para pesquisa de username
+            if (e.key === "Enter") {
+                //pode estar deprecated, mas funciona
+                pegarNick();
+            }
+        },
+        false
+    );
+
+    return (
+        <div className="completar">
+            <div id={tema}>
+                <img src={imagem} alt="React com Github Api" />
+                <div className="pesquisa">
+                    <p>Digite o username</p>
+                    <input src="text" required onChange={setPesquisa} />
+                    <button onClick={pegarNick}>PESQUISAR</button>
+                </div>
+                <div className="dadosEncontrados">
+                    <DadosEncontrados nickname={nickname} tema={tema} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Pesquisa;
